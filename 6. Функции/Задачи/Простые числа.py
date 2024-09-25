a07d8e723f83b977877715df146dac3ce885d49e
Никита <orekhov_2007@bk.ru>
2024-09-25 18:25:53 +0300

    Решение задач

--- 6. Функции/Задачи/Простые числа.py
+++ 6. Функции/Задачи/Простые числа.py
@@ -1,17 +1,13 @@
-def number(b):
-    a=[]
-    for i in range(2,b+1):
-        k=0
-        for j in range(1, i+1):
-            if i%j ==0:
-                k+=1
-        if k == 2:
-            a.append(i)
-    end=""
-    col=int(input("Введите число"))
-    g=a[:col]
-    print(g)
-number(1000)
-
-
-
+def is_simple(num):
+    for i in range(2, num):
+        if num%i == 0:
+            return False
+    return True
+a=int(input())
+count = 0
+b=2
+while count < a:
+    if is_simple(b):
+        print(f"{b}", end=" ")
+        count +=1
+    b +=1
